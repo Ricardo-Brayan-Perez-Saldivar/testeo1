2f3c51e11bc5f37134a4befb33e751baba131ec9
Ricardo-Brayan-Perez-Saldivar <ric_embroyo1999@hotmail.com>
2020-05-12 08:52:32 -0500

    agrege comentario

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let app: AppComponent;
+
+  beforeEach(async(() => {
+    app = new AppComponent();
+  }));
+
+  it('la suma debe dar 4',async(() => {
+    expect(app.add(2,2)).toEqual(4);
+  }));
+
+});
+//Para hacer otro comentario
+
+
